1a51712016eae9b12175b608045b9ab82c8af091
lookah-cmyk <merziluca89@gmail.com>
2026-07-15 17:18:06 +0100

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums: number[], val: number): number {
+        let k = 0;
+
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] !== val) {
+                nums[k] = nums[i];
+                k++;
+                console.log(k, nums);
+            }
+        }
+
+        return k;
+    }
+}
